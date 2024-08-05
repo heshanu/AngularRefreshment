36639a8b0da0ad604284335525d6977f300e62e8
heshanu <heshanu97@gmail.com>
2024-08-05 16:40:24 +0530

    not completed

--- src/app/rooms/rooms-booking/rooms-booking.component.ts
+++ src/app/rooms/rooms-booking/rooms-booking.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Route } from '@angular/router';
+import { RoomInterface } from '../../shared/interfaces/roomInterface';
+import { RoomService } from '../../service/room.service';
 
 @Component({
   selector: 'app-rooms-booking',
@@ -7,12 +9,24 @@ import { ActivatedRoute, Route } from '@angular/router';
   styleUrl: './rooms-booking.component.css'
 })
 export class RoomsBookingComponent implements OnInit{
-  constructor(private route: ActivatedRoute) { }
+
+  constructor(private route: ActivatedRoute,private roomService:RoomService) { }
   ngOnInit(): void {
     console.log(this.id);
+    this.getRoomDetails(this.id);
   }
   
+  roomDetail!:RoomInterface;
   id: number = this.route.snapshot.params['id']; 
   
+  public getRoomDetails(id:any){ 
+    this.roomService.getRoomDetails(id)
+    .subscribe((data:RoomInterface)=>{
+      this.roomDetail=data;
+    });  
+  }
   
+  bookRoom(id: number) {
+    
+  }
 }
